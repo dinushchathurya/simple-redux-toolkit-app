8a72bd39dd95bc29b9fe860c104e6b171a92c316
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-23 22:09:52 +0530

    feat: initialize the project

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useEffect } from 'react';
+import { getUser } from './redux/ducks/user';
+import { useSelector, useDispatch } from 'react-redux';
 
-function App() {
+export default function App() {
+  
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getUser());
+  }, [dispatch]);
+
+  const user = useSelector((state)=> state.user.user);
+  console.log(user);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+         Redux Saga Example
       </header>
     </div>
   );
 }
 
-export default App;
