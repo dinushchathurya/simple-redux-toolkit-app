e504fd515247fe21af55c508671668de0bd0689b
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-24 21:32:28 +0530

    refactor: replace user.js with userSlice.js

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -1,7 +1,7 @@
 import { takeLatest } from 'redux-saga/effects';
 import { handleGetUser } from './handlers/user';
-import { GET_USER } from '../ducks/user';
+import { getUser } from '../ducks/userSlice';
 
 export function* watcherSaga() {
-    yield takeLatest(GET_USER, handleGetUser);
+    yield takeLatest(getUser.type, handleGetUser);
 }
